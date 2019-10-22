5f8664b551f3f0608ad3645547a257d007569609
Romeo Rego <235005@student.pwr.edu.pl>
2019-10-22 19:21:46 +0200

    Updated bootstrap and removed unnecessary code in Startup

--- IssLocator/Startup.cs
+++ IssLocator/Startup.cs
@@ -11,7 +11,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using ReflectionIT.Mvc.Paging;
 
 namespace IssLocator
 {
@@ -47,7 +46,6 @@ namespace IssLocator
             services.AddMvc()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
                 .AddControllersAsServices();
-            services.AddPaging();
 
             services.AddScoped<IIssLocationService, IssLocationService>();
 
